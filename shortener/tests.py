355d772302a81303df59f32d4dd496edbfd1d64c
Erik Johansson <erik.johansson@dazy.se>
2023-09-29 00:33:33 +0200

    shortened url test

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.http import HttpRequest
+from django.contrib.auth.models import User
 from shortener.service import shortcode
+from shortener.models import Shorturl
 
 
 class ShortcodeTestCase(TestCase):
@@ -12,3 +16,24 @@ class ShortcodeTestCase(TestCase):
 
         # Assert that the length of the result is 5
         self.assertEqual(len(result), 5)
+
+
+class ShorturlDetailTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.shorturl = Shorturl.objects.create(
+            user=self.user, url="https://www.example.com"
+        )
+        self.url = reverse(
+            "shorturl_detail", kwargs={"identifier": self.shorturl.identifier}
+        )
+
+    def test_shorturl_detail(self):
+        request = HttpRequest()
+        request.user = self.user
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, self.shorturl.url)
