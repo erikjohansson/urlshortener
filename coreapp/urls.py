773a97b96841dc4d300d70524a417594186b08a2
Erik Johansson <erik.johansson@dazy.se>
2023-10-03 00:01:38 +0200

    remove pesky favicon 404

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -3,8 +3,9 @@ URL configuration for coreapp project.
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+from django.views.static import serve
 
 from shortener.views import home_view, shorturl_detail, shorturl_list, shorturl_delete
 
@@ -15,6 +16,11 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", shorturl_list, name="shorturl_list"),
     path("delete/", shorturl_delete, name="shorturl_delete"),
+    path(
+        "favicon.ico",
+        serve,
+        {"path": "img/favicon.ico", "document_root": settings.STATIC_ROOT},
+    ),
     path("<identifier>", shorturl_detail, name="shorturl_detail"),
 ]
 
